7e7d7236f681e8379ae95617c9afe52ca88a1a31
chris434 <christopherpearce589@gmail.com>
2023-12-27 11:55:52 +0000

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}

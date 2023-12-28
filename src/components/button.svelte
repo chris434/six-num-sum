a7c47bf8b37e53d7670dc259278a6e114092bdaa
chris434 <christopherpearce589@gmail.com>
2023-12-28 13:39:02 +0000

    feat: config bar toggle

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,3 +1,10 @@
-<button class="bg-teal-400 text-white p-3 rounded-md border-teal-400 border-2 hover:bg-white hover:text-teal-400">
+<script lang="ts">
+    export let onclick:()=>void=()=>{}
+    export let selected=false
+    export let className=''
+</script>
+
+
+<button on:click={onclick} class="bg-teal-400 p-3 rounded-md border-teal-400 border-2 hover:bg-white hover:text-teal-400 {className} {selected?'bg-white hover:bg-teal-400 hover:text-white text-teal-400':' text-white'}">
     <slot/>
 </button>
--- src/components/configBar.svelte
+++ src/components/configBar.svelte
@@ -2,9 +2,12 @@
 	import Button from "./button.svelte";
 	import Select from "./select.svelte";
     const options =['any','1-10','10-100','100-1000','1000+']
+    let configBarToggle =true
 
 </script>
-<section class="shadow-xl gap-5  flex items-center md:flex-row flex-col w-full p-5 justify-between">
+<section class="p-5 shadow-xl" >
+    <Button className='md:hidden block' selected={configBarToggle} onclick={()=> configBarToggle= !configBarToggle}>Config problem</Button>
+    <div class=" gap-5 items-center md:flex-row flex-col w-full justify-between mt-5 md:mt-0 {configBarToggle?'flex':'md:flex hidden'}">
        <div class="flex gap-5 items-center md:justify-normal xs:flex-row flex-col   justify-around md:w-auto w-full">
        <Select label="Sum" {options}/>
        <Select label="Target" {options}/>
@@ -14,6 +17,5 @@
         <div>or</div>
         <Button>Use same problem</Button>
     </div>
- 
-  
+    </div>
 </section>

7babf4b6cdac82fe95f6c035f5111fe5b9b73c35
chris434 <christopherpearce589@gmail.com>
2024-01-01 13:10:32 +0000

    featL: add number to sum

--- src/components/numberButtons.svelte
+++ src/components/numberButtons.svelte
@@ -3,9 +3,11 @@
 	import Button from "./button.svelte";
 
     const problem= getProblem()
+    const {addToSum}=problem
+$:console.log($problem)
 </script>
   <div class="grid xs:grid-cols-6 grid-cols-3 gap-5" >
-    {#each $problem.numbers as number }
-        <Button className="w-14 h-14">{number}</Button>
+    {#each $problem.numbers as {number,used},i}
+        <Button disabled={used} onclick={()=> addToSum(i,number)} className="w-14 h-14">{number}</Button>
     {/each}
 </div>

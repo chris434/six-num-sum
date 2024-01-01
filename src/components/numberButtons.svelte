cb582efab72eafffe218e6f06975febdf25c6352
chris434 <christopherpearce589@gmail.com>
2024-01-01 15:32:53 +0000

    feat: add operator

--- src/components/numberButtons.svelte
+++ src/components/numberButtons.svelte
@@ -8,6 +8,6 @@ $:console.log($problem)
 </script>
   <div class="grid xs:grid-cols-6 grid-cols-3 gap-5" >
     {#each $problem.numbers as {number,used},i}
-        <Button disabled={used} onclick={()=> addToSum(i,number)} className="w-14 h-14">{number}</Button>
+        <Button disabled={used} onclick={()=> addToSum(number,i)} className="w-14 h-14">{number}</Button>
     {/each}
 </div>

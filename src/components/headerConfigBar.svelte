5b2009dd929782626b2983a429b0efe9af1d8b8a
chris434 <christopherpearce589@gmail.com>
2024-01-21 13:57:43 +0000

    refactor: can reset on actions

--- src/components/headerConfigBar.svelte
+++ src/components/headerConfigBar.svelte
@@ -5,12 +5,17 @@ import {getProblem} from '../contexts/problemContext'
 const problem=getProblem()
 const {reset}=problem
 
+
+$:numberBeenClicked=$problem.numbers.some(numberObj => numberObj.used)
+$:answerHasBeenChecked= $problem.answerHasBeenChecked
+$:answerHasBeenShown= $problem.answerHasBeenShown
+$:canReset=  numberBeenClicked||answerHasBeenChecked||answerHasBeenShown?true:false
     
 </script>
 
 <div class="flex items-start gap-3 p-5 shadow-xl">
     <ConfigBar/>
-    {#if $problem.numbers.length}
+    {#if canReset}
     <Button onclick={reset}>rest</Button>
     {/if}
 </div>

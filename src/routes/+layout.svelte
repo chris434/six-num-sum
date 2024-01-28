eee2a7a6119af35dd5db41df128101ab869b95f3
chris434 <christopherpearce589@gmail.com>
2024-01-28 13:27:59 +0000

    refactor: inner layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
     import '../app.css'
 	import { setProblem } from '../contexts/problemContext'
-	import Header from '../components/header.svelte';
-	import Modal from '../components/modal.svelte';
-	import HeaderConfigBar from '../components/headerConfigBar.svelte';
+	import InnerLayout from '../components/innerLayout.svelte';
 
 	setProblem()
 </script>
-<div class="relitve">
-<div class="">
-<div class="sticky top-0 z-10 bg-white">
-<Header/>
-<HeaderConfigBar/>
-</div>
-<slot/>
-</div>
+<InnerLayout>
+	<slot/>
+</InnerLayout>
 
-<Modal />
-</div>
 

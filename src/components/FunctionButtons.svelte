edb5ab9c80ed3a2e9a33365c590da5a5b6891f5b
chris434 <christopherpearce589@gmail.com>
2024-01-28 12:18:54 +0000

    refactor: remove check answerbutton

--- src/components/FunctionButtons.svelte
+++ src/components/FunctionButtons.svelte
@@ -4,8 +4,8 @@
 const {clearSum,deleteLast,checkAnswer}=getProblem()
 </script>
 
-<section class="grid grid-cols-2 lg:grid-cols-4 w-full lg:text-nowrap gap-5 p-5">
+<section class="flex justify-center w-full lg:text-nowrap gap-5 p-5">
 <Button onclick={clearSum}>clear</Button>
 <Button onclick={deleteLast}>delete</Button>
-<Button onclick={checkAnswer}>check answer</Button>
 </section>
+
